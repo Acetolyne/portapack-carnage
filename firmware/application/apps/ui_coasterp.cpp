fcd5d4ff5231206af789220a7f943b9afea9cb5b
acetolyne <acetolyne@programmer.net>
2020-02-13 11:52:48 -0800

    Moved hex to decimal function to global include file

--- firmware/application/apps/ui_coasterp.cpp
+++ firmware/application/apps/ui_coasterp.cpp
@@ -40,20 +40,21 @@ using namespace portapack;
 
 namespace ui {
 
-unsigned int hex2int(char *hex) {
-    unsigned int val = 0;
-    while (*hex) {
-    // get current character then increment
-      unsigned int byte = *hex++;
-      // transform hex character to the 4bit equivalent number,using the ascii table indexes
-      if (byte >= '0' && byte <= '9') byte = byte - '0';
-      else if (byte >= 'a' && byte <='f') byte = byte - 'a' + 10;
-      else if (byte >= 'A' && byte <='F') byte = byte - 'A' + 10;
-      // shift 4 to make space for new digit, and add the 4 bits of the new digit
-        val = (val << 4) | (byte & 0xF);
-      }
-  return val;
-}
+//@todo make this available globally via string_format.cpp
+//unsigned int hex2int(char *hex) {
+//    unsigned int val = 0;
+//    while (*hex) {
+//    // get current character then increment
+//      unsigned int byte = *hex++;
+//      // transform hex character to the 4bit equivalent number,using the ascii table indexes
+//      if (byte >= '0' && byte <= '9') byte = byte - '0';
+//      else if (byte >= 'a' && byte <='f') byte = byte - 'a' + 10;
+//      else if (byte >= 'A' && byte <='F') byte = byte - 'A' + 10;
+//      // shift 4 to make space for new digit, and add the 4 bits of the new digit
+//        val = (val << 4) | (byte & 0xF);
+//      }
+//  return val;
+//}
 
 std::vector<std::string> Split(const std::string& str)
 {
